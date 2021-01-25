b747fb363d1d9b909670707338492ea9d6835aca
unknown <anthonyoliai@gmail.com>
2021-01-25 17:05:03 +0100

    Added Products main page frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import { useEffect } from 'react'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+
 const App = () => {
   return (
     <>
       <Header></Header>
       <main className='py-3'>
         <Container>
+          <HomeScreen></HomeScreen>
           <h1>welcome to ProShop</h1>
         </Container>
       </main>
