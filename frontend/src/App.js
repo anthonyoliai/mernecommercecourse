314b010be91343240b768665f52a4468177ae03f
unknown <anthonyoliai@gmail.com>
2021-01-29 00:45:00 +0100

    Support for changing profiles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,9 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
-
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
 const App = () => {
   return (
     <Router>
@@ -14,9 +16,12 @@ const App = () => {
       <Header></Header>
       <main className='py-3'>
         <Container>
+          <Route path='/register' component={RegisterScreen} exact></Route>
+          <Route path='/login' component={LoginScreen} exact></Route>
           <Route path='/' component={HomeScreen} exact></Route>
           <Route path='/product/:id' component={ProductScreen}></Route>
           <Route path='/cart/:id?' component={CartScreen}></Route>
+          <Route path='/profile' component={ProfileScreen}></Route>
         </Container>
       </main>
       <Footer></Footer>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import { LinkContainer } from 'react-router-bootstrap'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import { useSelector, useDispatch } from 'react-redux'
+import { logout } from '../actions/userActions'
 const Header = () => {
+  const userLogin = useSelector((state) => state.userLogin)
+  const dispatch = useDispatch()
+  const { userInfo } = userLogin
+
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
@@ -17,12 +26,22 @@ const Header = () => {
                   <i className='fas fa-shopping-cart'>Cart</i>{' '}
                 </Nav.Link>
               </LinkContainer>
-
-              <LinkContainer to='/login'>
-                <Nav.Link>
-                  <i className='fas fa-user'>Sign in</i>
-                </Nav.Link>
-              </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to='/login'>
+                  <Nav.Link>
+                    <i className='fas fa-user'>Sign in</i>
+                  </Nav.Link>
+                </LinkContainer>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
