42278eeb2939b096d3f7628a7421e5e92395017f
unknown <anthonyoliai@gmail.com>
2021-02-02 00:49:56 +0100

    Finished Admin support and added multer upload support

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
 import ProductListScreen from './screens/ProductListScreen'
+import ProductEditScreen from './screens/ProductEditScreen'
 const App = () => {
   return (
     <Router>
@@ -37,6 +38,10 @@ const App = () => {
           <Route
             path='/admin/productlist'
             component={ProductListScreen}
+          ></Route>
+          <Route
+            path='/admin/product/:id/edit'
+            component={ProductEditScreen}
           ></Route>
           <Route path='/' component={HomeScreen} exact></Route>
         </Container>
